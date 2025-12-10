31842c2e9e13510f93e037bdd9a5bbdf0918bbfa
Chidera001-dev <nnamanichideraemmanuel7@gmail.com>
2025-12-10 14:05:33 +0100

    Update email configuration to use default values for backend and host

--- chidera_store/settings.py
+++ chidera_store/settings.py
@@ -109,8 +109,8 @@ DATABASES = {
         "PORT": config("POSTGRES_PORT", cast=int),
     }
 }
-EMAIL_BACKEND = config("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
-EMAIL_HOST = config("EMAIL_HOST", "localhost")
+EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
+EMAIL_HOST = config("EMAIL_HOST", default="localhost")
 EMAIL_PORT = config("EMAIL_PORT", cast=int, default=1025)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=False)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
